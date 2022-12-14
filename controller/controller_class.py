2a0838a5a9975fbf2c0ac49e6be65c45ef23a769
IllTushar <gtushar7668@gmail.com>
2022-12-15 00:48:02 +0530

    4th commit

--- controller/controller_class.py
+++ controller/controller_class.py
@@ -1,6 +1,7 @@
 from app import app
 from flask import request
 from model.model_response import model_class
+from datetime import datetime
 obj = model_class()
 @app.route("/user/put",methods=["PUT"])
 def update_controller():
@@ -8,3 +9,24 @@ def update_controller():
 @app.route("/user/delete/<id>",methods=["DELETE"])
 def delete_controller(id):
     return obj.delete_mode_method(id)
+@app.route("/user/getall")
+def get_all_details():
+    return obj.get_all_data()
+@app.route("/user/post",methods=["POST"])
+def get_post_data():
+    return obj.post_details(request.form)
+@app.route("/user/patch",methods=["PATCH"])
+def patch_data():
+    return obj.patch_the_data(request.form)
+@app.route("/user/getall/limit/<limit>/page/<page>")
+def get_all_limit_data(limit,page):
+    return obj.pagination_all_data(limit,page)
+@app.route("/user/<uid>/upload/avatar",methods=["PUT"])
+def user_upload_avatar_control(uid):
+     file  = request.files['avatar']
+     uniqueFileName = str(datetime.now().timestamp()).replace(".", "")
+     fileNameSplit = file.filename.split(".")
+     ext = fileNameSplit[len(fileNameSplit)-1]
+     finalPath = f"upload/{uniqueFileName}.{ext}"
+     file.save(finalPath)
+     return obj.upload_images(uid,finalPath)
--- model/model_response.py
+++ model/model_response.py
@@ -1,4 +1,5 @@
 import mysql.connector
+from flask import make_response
 class model_class():
     def __init__(self):
         try:
@@ -11,13 +12,58 @@ class model_class():
     def update_mode_method(self,data):
         self.cur.execute(f"UPDATE user_info SET name='{data['name']}', email='{data['email']}', password='{data['password']}' WHERE id ='{data['id']}';")
         if self.cur.rowcount>0:
-            return "Update Suceesfull"
+            return make_response({"message":"Update successful"},201)
         else:
-            return "Unsuccessful"
+            return make_response({"message":"Nothing to update"},202)
 
     def delete_mode_method(self,data):
         self.cur.execute(f"DELETE FROM user_info WHERE id = {data}")
         if self.cur.rowcount>0:
-            return "delete Suceesfull"
+            return make_response({"message":"Deletion successful"},202)
         else:
-            return "Unsuccessful"
+            return make_response({"message":"Deletion unsccessful"},204)
+
+
+    def get_all_data(self):
+        self.cur.execute("SELECT * FROM user_info")
+        result = self.cur.fetchall()
+        if len(result)>0:
+            return make_response({"data":result},200)
+        else:
+            return make_response({"message":"something went wrong"},204)
+
+
+    def post_details(self,data):
+        self.cur.execute(f"INSERT INTO user_info(id,name,email,password) VALUES('{data['id']}','{data['name']}','{data['email']}','{data['password']}');")
+        return make_response({"message":"data uploaded"},200)
+
+    def patch_the_data(self,data):
+        qry="UPDATE user_info SET"
+        for key in data:
+            qry +=f" {key}='{data[key]}',"
+        qry = qry[:-1]+f" WHERE id = {data['id']}"
+        self.cur.execute(qry)
+        if self.cur.rowcount>0:
+            return make_response({"message":"updation successful"},201)
+        else:
+            return make_response({"message":"Nothing to update"},202)
+
+    def pagination_all_data(self,limit,page):
+        limit = int(limit)
+        page = int(page)
+        start = (limit*page)-limit
+        self.cur.execute(f"SELECT * FROM user_info LIMIT  {start} ,{limit} ")
+        result = self.cur.fetchall()
+        if self.cur.rowcount>0:
+            return make_response({"payload":result},200)
+        else:
+            return make_response({"message":"no record found"},204)
+
+    def upload_images(self,uid,data):
+        self.cur.execute(f"UPDATE user_info SET avatar='{data} where id = {uid}'")
+        # result = self.cur.fetchall()
+        if self.cur.rowcount>0:
+            return make_response({"message":"update successfull"},201)
+        else:
+            return make_response({"message":"no data found"},202)
+
